1a33d26ee266b66fb24cbe7f9fc8891283d202db
Cihan Yakar <cihanyakar@gmail.com>
2019-01-30 11:03:08 +0300

    brunch kaldırıldı düz webpack e geçildi, klasör yapısı değiştirildi

--- Honoplay/AdminFrontEnd/server.js
+++ Honoplay/AdminFrontEnd/server.js
@@ -10,19 +10,9 @@ const exec = util.promisify(require('child_process').execSync);
 const opn = require('opn');
 
 console.log("Version. 1.0");
-
-async function runServer() {
-
-    opn('http://localhost:3333/', { app: ['chrome', '--disable-plugins', '--remote-debugging-port=9222', '--new-window', '--disable-translate'] }).then(() => {
+(async function () {
+    opn('http://localhost:3000/', { app: ['chrome', '--disable-plugins', '--remote-debugging-port=9222', '--new-window', '--disable-translate', '--virtual-time-budget=10000'] }).then(() => {
         console.log(`browser started`);
     });
-
-    const { stdout, stderr } = await exec('yarn start', { stdio: 'inherit' });
-
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-}
-runServer();
-
-
-
+    await exec('yarn start', { stdio: 'inherit' });
+})();
