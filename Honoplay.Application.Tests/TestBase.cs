04532d84fd74144c5b36908c2bad1f9ca9eca10d
Cihan Yakar <cihanyakar@gmail.com>
2019-02-12 17:45:23 +0300

    App test

---
+++ Honoplay.Application.Tests/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using Honoplay.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable enable
+namespace Honoplay.Application.Tests
+{
+    public abstract class TestBase
+    {
+
+        protected HonoplayDbContext GetDbContext()
+        {
+            var builder = new DbContextOptionsBuilder<HonoplayDbContext>();
+            var dbContext = new HonoplayDbContext(builder.Options);
+
+            dbContext.Database.EnsureCreated();
+            return dbContext;
+        }
+    }
+}
---
+++ Honoplay.Application.Tests/Tokens/GetAdminUserTokenCommandTest.cs
@@ -0,0 +1,53 @@
+﻿using Honoplay.Domain.Entities;
+using Honoplay.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honoplay.Application.Tests.Tokens
+{
+    public abstract class GetAdminUserTokenCommandTest : TestBase, IDisposable
+    {
+        private readonly HonoplayDbContext _context;
+
+        public GetAdminUserTokenCommandTest()
+        {
+            _context = InitAndGetDbContext();
+        }
+        private HonoplayDbContext InitAndGetDbContext()
+        {
+            var context = GetDbContext();
+
+            var tenantGuid = Guid.NewGuid();
+
+            context.Tenants.Add(new Tenant
+            {
+                Id = tenantGuid,
+                Name = "TestTenant#01"
+            });
+
+            context.AdminUsers.Add(new AdminUser
+            {
+                TenantId = tenantGuid,
+                Username = "TestAdminUser#01",
+                Password = new byte[] { },
+                PasswordSalt = new byte[] { },
+                LastPasswordChangeDateTime = DateTime.Today.AddDays(-5)
+            });
+
+            context.SaveChanges();
+
+            return context;
+        }
+
+        public void Dispose()
+        {
+            if (_context is null)
+            {
+                return;
+            }
+            _context.Dispose();
+        }
+    }
+}
