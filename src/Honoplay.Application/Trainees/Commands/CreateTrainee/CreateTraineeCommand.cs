5074bea19d7c42fc2d9eefdb6745c58c29b96e77
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-05-02 17:34:49 +0300

    CreateTraineeCommand eklemeler yapildi

--- src/Honoplay.Application/Trainees/Commands/CreateTrainee/CreateTraineeCommand.cs
+++ src/Honoplay.Application/Trainees/Commands/CreateTrainee/CreateTraineeCommand.cs
@@ -8,5 +8,12 @@ namespace Honoplay.Application.Trainees.Commands.CreateTrainee
 {
     public class CreateTraineeCommand : IRequest<ResponseModel<CreateTraineeModel>>
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string NationalIdentityNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Gender { get; set; }
+        public int WorkingStatusId { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
