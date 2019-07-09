54ccc184dd1076fb9765f0bbc701c5eade37fc59
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-09 14:44:46 +0300

    WithMessage içine yazılacak Terasu KEY lerinin tek bir yerden yönetilebilmesi, yazım yanlışlarından korunabilmek  ve intellisense kullanılabilmesi için MessageCodes classı oluşturularak const olarak tanımlanmıştır. AdminUser için validatorler düzenlenmiştir.

--- src/Honoplay.Application/AdminUsers/Commands/UpdateAdminUser/UpdateAdminUserValidator.cs
+++ src/Honoplay.Application/AdminUsers/Commands/UpdateAdminUser/UpdateAdminUserValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Honoplay.FluentValidatorJavascript;
 
 namespace Honoplay.Application.AdminUsers.Commands.UpdateAdminUser
 {
@@ -6,10 +7,23 @@ namespace Honoplay.Application.AdminUsers.Commands.UpdateAdminUser
     {
         public UpdateAdminUserValidator()
         {
-            RuleFor(x => x.Email).MaximumLength(150).NotNull().EmailAddress().NotEmpty();
-            RuleFor(x => x.Password).MinimumLength(6).MaximumLength(50).NotNull().NotEmpty();
-            RuleFor(x => x.Name).MaximumLength(50).NotNull().NotEmpty();
-            RuleFor(x => x.Surname).MaximumLength(50).NotNull().NotEmpty();
+            RuleFor(x => x.Email).NotNull().WithMessage(MessageCodes.NotNullValidator);
+            RuleFor(x => x.Email).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
+            RuleFor(x => x.Email).EmailAddress().WithMessage(MessageCodes.EmailValidator);
+            RuleFor(x => x.Email).MaximumLength(150).WithMessage(MessageCodes.MaximumLengthValidator);
+
+            RuleFor(x => x.Password).NotNull().WithMessage(MessageCodes.NotNullValidator);
+            RuleFor(x => x.Password).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
+            RuleFor(x => x.Password).MinimumLength(6).WithMessage(MessageCodes.MinimumLengthValidator);
+            RuleFor(x => x.Password).MaximumLength(50).WithMessage(MessageCodes.MaximumLengthValidator);
+
+            RuleFor(x => x.Name).NotNull().WithMessage(MessageCodes.NotNullValidator);
+            RuleFor(x => x.Name).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage(MessageCodes.MaximumLengthValidator);
+
+            RuleFor(x => x.Surname).NotNull().WithMessage(MessageCodes.NotNullValidator);
+            RuleFor(x => x.Surname).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
+            RuleFor(x => x.Surname).MaximumLength(50).WithMessage(MessageCodes.MaximumLengthValidator);
         }
     }
 
---
+++ src/Honoplay.FluentValidatorJavascript/MessageCodes.cs
@@ -0,0 +1,13 @@
+﻿namespace Honoplay.FluentValidatorJavascript
+{
+    public static class MessageCodes
+    {
+        public const string NotNullValidator = "NotNullValidator";
+        public const string NotEmptyValidator = "NotEmptyValidator";
+        public const string EmailValidator = "EmailValidator";
+        public const string CreditCardValidator = "CreditCardValidator";
+        public const string MinimumLengthValidator = "MinimumLengthValidator";
+        public const string MaximumLengthValidator = "MaximumLengthValidator";
+        public const string InclusiveBetweenValidator = "InclusiveBetweenValidator";
+    }
+}
