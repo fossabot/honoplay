09dfe3ff87c5803059e406f2d5648335b247547c
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-10 16:28:12 +0300

    AdminUser için validatorler düzenlendi.

--- src/Honoplay.Application/AdminUsers/Commands/AuthenticateAdminUser/AuthenticateAdminUserValidator.cs
+++ src/Honoplay.Application/AdminUsers/Commands/AuthenticateAdminUser/AuthenticateAdminUserValidator.cs
@@ -7,15 +7,18 @@ namespace Honoplay.Application.AdminUsers.Commands.AuthenticateAdminUser
     {
         public AuthenticateAdminUserValidator()
         {
-            RuleFor(x => x.Email).NotNull().WithMessage(MessageCodes.NotNullValidator);
-            RuleFor(x => x.Email).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
-            RuleFor(x => x.Email).EmailAddress().WithMessage(MessageCodes.EmailValidator);
-            RuleFor(x => x.Email).MaximumLength(150).WithMessage(MessageCodes.MaximumLengthValidator);
 
-            RuleFor(x => x.Password).NotNull().WithMessage(MessageCodes.NotNullValidator);
-            RuleFor(x => x.Password).NotEmpty().WithMessage(MessageCodes.NotEmptyValidator);
-            RuleFor(x => x.Password).MinimumLength(6).WithMessage(MessageCodes.MinimumLengthValidator);
-            RuleFor(x => x.Password).MaximumLength(50).WithMessage(MessageCodes.MaximumLengthValidator);
+            RuleFor(x => x.Email)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(150);
+
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .MinimumLength(6)
+                .MaximumLength(50);
         }
     }
 
