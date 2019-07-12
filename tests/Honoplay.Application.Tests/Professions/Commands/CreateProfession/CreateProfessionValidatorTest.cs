1d87404014a7529dbda41c569c0af76d689153ee
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-12 14:28:48 +0300

    Profession için GetList ve Create unit testleri yazıldı.

---
+++ tests/Honoplay.Application.Tests/Professions/Commands/CreateProfession/CreateProfessionValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Honoplay.Application.Tests.Professions.Commands.CreateProfession
+{
+    public class CreateProfessionValidatorTest
+    {
+        private readonly CreateProfessionValidator _validator;
+        public CreateProfessionCommandValidatorTest()
+        {
+            _validator = new CreateProfessionValidator();
+        }
+
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_validator.Validate(
+                new CreateProfessionCommand
+                {
+                    AdminUserId = 1,
+                    TenantId = new Guid(),
+                    Professions = new List<string> { "yazilim", "tasarim" }
+                }
+                ).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Professions, new List<string>());
+        }
+    }
+}
