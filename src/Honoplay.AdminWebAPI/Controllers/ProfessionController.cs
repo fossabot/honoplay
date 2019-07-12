34f686c2c9cb868c14af279a90646a54d4419d40
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-12 16:08:09 +0300

    ProfessionController eklendi.

---
+++ src/Honoplay.AdminWebAPI/Controllers/ProfessionController.cs
@@ -0,0 +1,75 @@
+﻿using Honoplay.Application._Infrastructure;
+using Honoplay.Application.Professions.Commands.CreateProfession;
+using Honoplay.Application.Professions.Queries.GetProfessionsList;
+using Honoplay.Common._Exceptions;
+using Honoplay.Common.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Honoplay.AdminWebAPI.Controllers
+{
+    [Authorize]
+    public class ProfessionController : BaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<CreateProfessionModel>>> Post([FromBody]CreateProfessionCommand command)
+        {
+            try
+            {
+                var userId = Claims[ClaimTypes.Sid].ToInt();
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                command.AdminUserId = userId;
+                command.TenantId = tenantId;
+
+                var createProfessionModel = await Mediator.Send(command);
+                return Ok(createProfessionModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ObjectAlreadyExistsException ex)
+            {
+                return Conflict(new ResponseModel<CreateProfessionModel>(new Error(HttpStatusCode.Conflict, ex)));
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<ProfessionsListModel>>> Get([FromQuery] GetProfessionsListQueryModel query)
+        {
+            try
+            {
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var professionsListModel = await Mediator.Send(new GetProfessionsListQuery(tenantId, query.Skip, query.Take));
+
+                return Ok(professionsListModel);
+
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+    }
+}
