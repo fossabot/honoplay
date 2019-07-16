7d40b6bbf39a62906025c0cd760384beb521c3a9
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-16 14:48:18 +0300

    Question ve Answer tabloları db ye eklendi. Configler düzenlendi.

---
+++ src/Honoplay.Persistence/Configurations/QuestionConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Honoplay.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Honoplay.Persistence.Configurations
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            //Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            //Text
+            builder.Property(x => x.Text)
+                .HasMaxLength(500);
+
+            //RELATIONS
+
+            //Tenant
+            builder.HasOne(q => q.Tenant)
+                .WithMany(t => t.Questions)
+                .HasForeignKey(q => q.TenantId);
+
+
+        }
+    }
+}
