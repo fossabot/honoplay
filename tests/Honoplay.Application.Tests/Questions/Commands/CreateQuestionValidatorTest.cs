79eb811867ce104cf4d8d63d8da28449241b17c9
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-17 14:03:30 +0300

    QuestionController ve CreateQuestion için testler yazıldı. CreateDepartment için refactoring yapıldı.

---
+++ tests/Honoplay.Application.Tests/Questions/Commands/CreateQuestionValidatorTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Honoplay.Application.Tests.Questions.Commands
+{
+    public class CreateQuestionValidatorTest
+    {
+        private readonly CreateQuestionValidator _createQuestionValidator;
+
+        public CreateQuestionValidatorTest()
+        {
+            _createQuestionValidator = new CreateQuestionValidator();
+        }
+
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_createQuestionValidator.Validate(
+                new CreateQuestionCommand
+                {
+                    Text = "Asagidakilerden hangisi asagidadir?",
+                    Duration = 123
+                }
+            ).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _createQuestionValidator.ShouldHaveValidationErrorFor(x => x.Text, string.Empty);
+            _createQuestionValidator.ShouldHaveValidationErrorFor(x => x.Duration, 0);
+        }
+    }
+}
