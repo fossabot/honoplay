2628b7cf3f492a8a9ad4bbb88ad003751a65f12f
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-18 10:34:46 +0300

    QuestionController eklendi ver servis metotları ile ilişkiler kuruldu.

---
+++ src/Honoplay.AdminWebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,148 @@
+﻿using Honoplay.Application._Infrastructure;
+using Honoplay.Application.Questions.Commands.CreateQuestion;
+using Honoplay.Application.Questions.Commands.UpdateQuestion;
+using Honoplay.Application.Questions.Queries.GetQuestionDetail;
+using Honoplay.Application.Questions.Queries.GetQuestionsList;
+using Honoplay.Common._Exceptions;
+using Honoplay.Common.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Honoplay.AdminWebAPI.Controllers
+{
+    [Authorize]
+    public class QuestionController : BaseController
+    {
+        /// <summary>
+        /// This service create question.
+        /// </summary>
+        /// <param name="command">Create question model</param>
+        /// <returns>CreateQuestionModel with status code.</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<CreateQuestionModel>>> Post([FromBody]CreateQuestionCommand command)
+        {
+            try
+            {
+                command.CreatedBy = Claims[ClaimTypes.Sid].ToInt();
+                command.TenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var createQuestionModel = await Mediator.Send(command);
+
+                return Created($"api/question/{createQuestionModel.Items.Single().Text}", createQuestionModel);
+            }
+            catch (ObjectAlreadyExistsException ex)
+            {
+                return Conflict(new ResponseModel<CreateQuestionModel>(new Error(HttpStatusCode.Conflict, ex)));
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+        /// <summary>
+        /// This service update question.
+        /// </summary>
+        /// <param name="command">Update question model</param>
+        /// <returns>UpdateQuestionModel with status code.</returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ResponseModel<UpdateQuestionModel>>> Put([FromBody]UpdateQuestionCommand command)
+        {
+            try
+            {
+                command.UpdatedBy = Claims[ClaimTypes.Sid].ToInt();
+                command.TenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var updateQuestionModel = await Mediator.Send(command);
+
+                return Ok(updateQuestionModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ObjectAlreadyExistsException ex)
+            {
+                return Conflict(new ResponseModel<UpdateQuestionModel>(new Error(HttpStatusCode.Conflict, ex)));
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+
+        /// <summary>
+        /// This service update question.
+        /// </summary>
+        /// <param name="query">GetAll question model</param>
+        /// <returns>Get all questions list by tenant id with status code.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<QuestionsListModel>>> Get([FromQuery]GetQuestionsListQueryModel query)
+        {
+            try
+            {
+                var userId = Claims[ClaimTypes.Sid].ToInt();
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var questionsListModel = await Mediator.Send(new GetQuestionsListQuery(userId, tenantId, query.Skip, query.Take));
+
+                return Ok(questionsListModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+
+        /// <summary>
+        /// This service update question.
+        /// </summary>
+        /// <param name="id">Get question model</param>
+        /// <returns>Get question by tenant id and question id with status code.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<QuestionsListModel>>> Get(int id)
+        {
+            try
+            {
+                var userId = Claims[ClaimTypes.Sid].ToInt();
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var questionsListModel = await Mediator.Send(new GetQuestionDetailQuery(id, userId, tenantId));
+
+                return Ok(questionsListModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+    }
+}
