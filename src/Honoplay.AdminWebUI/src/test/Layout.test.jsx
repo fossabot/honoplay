10e7f8f6235beb4f9131e0796337d5bfa2f35e60
sadumankucuk <sadumankucuk@gmail.com>
2019-07-26 16:35:47 +0300

    Layout ve Table test güncellendi

--- src/Honoplay.AdminWebUI/src/test/Layout.test.jsx
+++ src/Honoplay.AdminWebUI/src/test/Layout.test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'jsdom-global/register'; 
+import 'jsdom-global/register';
 import should from 'should';
 import { expect } from 'chai';
-import sinon from 'sinon';
 import { configure, shallow, mount, render } from 'enzyme';
-import { init } from '@omegabigdata/terasu-api-proxy';
 
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
@@ -15,12 +13,12 @@ import TrainerCard from '../components/Layout/TrainerCard';
 import List from '../components/Layout/ListItemComponent';
 import Layout from '../components/Layout/LayoutComponent';
 
-import {ListItem, IconButton} from '@material-ui/core';
+import { ListItem, IconButton } from '@material-ui/core';
 
 describe('<CompanyCard/>', () => {
     it('Should have props for companyName', () => {
         const wrapper = shallow(<CompanyCard
-            companyName = "Framer Bilişim Teknolojileri"
+            companyName="Framer Bilişim Teknolojileri"
         />);
         wrapper.props().companyName.should.be.defined;
     });
@@ -29,7 +27,7 @@ describe('<CompanyCard/>', () => {
 describe('<TrainerCard/>', () => {
     it('Should have props  trainerName', () => {
         const wrapper = shallow(<TrainerCard
-            trainerName = "Ahmet Frankfurt"
+            trainerName="Ahmet Frankfurt"
         />);
         wrapper.props().trainerName.should.be.defined;
     });
@@ -38,32 +36,31 @@ describe('<TrainerCard/>', () => {
 describe('<List/>', () => {
     it('Should have props  pageLink, pageIcon and pageName', () => {
         const wrapper = shallow(<List
-            pageLink = "/home/sorular"
-            pageIcon = "question-circle"
-            pageName = "Sorular"
+            pageLink="/home/sorular"
+            pageIcon="question-circle"
+            pageName="Sorular"
         />);
         wrapper.props().pageLink.should.be.defined;
         wrapper.props().pageIcon.should.be.defined;
         wrapper.props().pageName.should.be.defined;
     });
 
-    // it('Should change class when ListItem click', () => {
-    //     const wrapper = shallow(<List
-    //         pageLink = "/home/sorular"
-    //         pageIcon = "question-circle"
-    //         pageName = "Sorular"
-    //     />).dive();
-    //     wrapper.find(ListItem).simulate('click');
-    //     expect(wrapper.find(ListItem).prop('activeClassName'))
-    //     .to.equal('ListItemComponent-active-90');
-    // });
+    it('Should change class when ListItem click', () => {
+        const wrapper = shallow(<List
+            pageLink="/home/sorular"
+            pageIcon="question-circle"
+            pageName="Sorular"
+        />).dive();
+        wrapper.find(ListItem).simulate('click');
+        expect(wrapper.find(ListItem).prop('activeClassName')).to.equal('ListItemComponent-active-240');;
+    });
 })
 
 describe('<Layout/>', () => {
     it('should open drawer when mobil menu icon click', () => {
-        const wrapper = shallow(<Layout/>).dive();
+        const wrapper = shallow(<Layout />).dive();
         wrapper.find(IconButton).simulate('click');
-        wrapper.setState({mobileOpen: true});
+        wrapper.setState({ mobileOpen: true });
         expect(wrapper.state().mobileOpen).to.equal(true);
     });
 })
