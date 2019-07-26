9b1f8ad6448f2a606b2690026cda926a396b675f
sadumankucuk <sadumankucuk@gmail.com>
2019-07-26 09:50:26 +0300

    Button test güncellendi

--- src/Honoplay.AdminWebUI/src/test/Button.test.jsx
+++ src/Honoplay.AdminWebUI/src/test/Button.test.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'jsdom-global/register'; 
+import 'jsdom-global/register';
 import should from 'should';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { configure, shallow, mount, render } from 'enzyme';
-import { init } from '@omegabigdata/terasu-api-proxy';
+import { configure, shallow, mount } from 'enzyme';
 
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-import Button from '../components/Button/ButtonComponent';
+import ButtonComponent from '../components/Button/ButtonComponent';
+import { Button } from '@material-ui/core';
 
 describe('<Button/>', () => {
     it('should have props for data, buttonColor, buttonName, onClick ', () => {
         const onClick = sinon.spy();
-        const wrapper = shallow(<Button
-            buttonColor = "primary"
-            buttonName = 'Save'
-            onClick = {onClick}
+        const wrapper = shallow(<ButtonComponent
+            buttonColor="primary"
+            buttonName='Save'
+            onClick={onClick}
         />);
         wrapper.props().buttonColor.should.be.defined;
-        wrapper.props().buttonName.should.be.defined; 
+        wrapper.props().buttonName.should.be.defined;
         wrapper.props().onClick.should.be.defined;
     });
 
+    it('should invoke onClick callback when click to "Button"', () => {
+        const onClick = sinon.spy();
+        const wrapper = mount(<ButtonComponent
+            buttonColor="primary"
+            buttonName='Save'
+            onClick={onClick}
+        />);
+        wrapper.find(Button).simulate('click');
+        expect(onClick.calledOnce).to.equal(true);
+    })
+
+    it('should not do anything when clicked because the button is disabled', () => {
+        const onClick = sinon.spy();
+        const wrapper = mount(<ButtonComponent
+            buttonColor="primary"
+            buttonName='Save'
+            onClick={onClick}
+            disabled
+        />);
+        wrapper.find(Button).simulate('click');
+        expect(wrapper.find(Button).prop('disabled')).to.equal(true);
+    })
 })
