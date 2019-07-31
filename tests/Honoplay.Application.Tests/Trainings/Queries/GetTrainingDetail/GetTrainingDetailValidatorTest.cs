6138a42e3b7dab925258107d83c8c9eae159e091
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-07-31 16:41:34 +0300

    Training için GetDetail unit testleri eklendi.

---
+++ tests/Honoplay.Application.Tests/Trainings/Queries/GetTrainingDetail/GetTrainingDetailValidatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace Honoplay.Application.Tests.Trainings.Queries.GetTrainingDetail
+{
+    public class GetTrainingDetailValidatorTest
+    {
+        private readonly GetTrainingDetailValidator _validator;
+
+        public GetTrainingDetailValidatorTest()
+        {
+            _validator = new GetTrainingDetailValidator();
+        }
+
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_validator.Validate(new GetTrainingDetailQuery(adminUserId: 1, id: 1, tenantId: Guid.NewGuid())).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x.Id, 0);
+        }
+    }
+}
