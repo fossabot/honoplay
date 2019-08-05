0b3c04bc7ea89c73cbcc773bbe4026ad879c3cf2
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-05 13:25:26 +0300

    Session için API lerin integration testleri yazıldı.

---
+++ tests/Honoplay.System.Tests/Controllers/SessionControllerIntegrationTests.cs
@@ -0,0 +1,113 @@
+﻿using Honoplay.AdminWebAPI;
+using Honoplay.Common.Constants;
+using Honoplay.System.Tests.Extensions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Honoplay.System.Tests.Controllers
+{
+    public class SessionControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public SessionControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanCreateSessionAsync()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var createSessionCommand = new CreateSessionCommand
+            {
+                CreateSessionModels = new List<CreateSessionCommandModel>
+                {
+                    new CreateSessionCommandModel
+                    {
+                        ClassroomId = 1,
+                        GameId = 1,
+                        Name = "test"
+                    }
+                }
+            };
+
+            var serializedSessionCommand = JsonConvert.SerializeObject(createSessionCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PostAsync(requestUri: "api/Session",
+                content: new StringContent(content: serializedSessionCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanUpdateSession()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var updateSessionCommand = new UpdateSessionCommand
+            {
+                Id = 1,
+                GameId = 1,
+                ClassroomId = 1,
+                Name = "test"
+            };
+
+            var serializedUpdateCommand = JsonConvert.SerializeObject(updateSessionCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PutAsync(requestUri: "api/Session",
+                    content: new StringContent(content: serializedUpdateCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            //Must be successful
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetSessionsList()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var getSessionsListQuery = new GetSessionsListQueryModel
+            {
+                Skip = 0,
+                Take = 10
+            };
+
+            var httpResponse = await authorizedClient.GetAsync(
+                requestUri: $"api/Session?Skip={getSessionsListQuery.Skip}&Take={getSessionsListQuery.Take}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetSessionDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"api/Session/1");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+    }
+}
