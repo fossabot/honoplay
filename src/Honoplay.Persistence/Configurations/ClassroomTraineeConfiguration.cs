c6330e53e4f8e03d35cb8d55fc37201174de0541
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-08 11:34:11 +0300

    Classroom ile Trainee arasında çoka çok ilişki kuruldu.

---
+++ src/Honoplay.Persistence/Configurations/ClassroomTraineeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Honoplay.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Honoplay.Persistence.Configurations
+{
+    public class ClassroomTraineeConfiguration : IEntityTypeConfiguration<ClassroomTrainee>
+    {
+        public void Configure(EntityTypeBuilder<ClassroomTrainee> builder)
+        {
+            //Tenant
+            builder.HasOne(x => x.Classroom)
+                .WithMany(x => x.ClassroomTrainees)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(x => x.ClassroomId)
+                .HasConstraintName("FK_Classroom_ClassroomTrainee");
+
+            //AdminUser
+            builder.HasOne(x => x.Trainee)
+                .WithMany(x => x.ClassroomTrainees)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasForeignKey(x => x.Trainee)
+                .HasConstraintName("FK_Trainee_ClassroomTrainee");
+        }
+    }
+}
