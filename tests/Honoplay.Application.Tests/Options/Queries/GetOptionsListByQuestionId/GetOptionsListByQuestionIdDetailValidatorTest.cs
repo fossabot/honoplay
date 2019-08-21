a755b27fad77a8f9c4e98d909fb5bfe2f0982e0e
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-21 16:47:36 +0300

    GetOptionsListByQuestionId için servis metotları için unit testler yazıldı.

---
+++ tests/Honoplay.Application.Tests/Options/Queries/GetOptionsListByQuestionId/GetOptionsListByQuestionIdDetailValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using Honoplay.Application.Options.Queries.GetOptionsListByQuestionId;
+using System;
+using Xunit;
+
+namespace Honoplay.Application.Tests.Options.Queries.GetOptionsListByQuestionId
+{
+    public class GetOptionsListByQuestionIdValidatorTest
+    {
+        private readonly GetOptionsListByQuestionIdValidator _getOptionsListByQuestionIdValidator;
+
+        public GetOptionsListByQuestionIdValidatorTest()
+        {
+            _getOptionsListByQuestionIdValidator = new GetOptionsListByQuestionIdValidator();
+        }
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_getOptionsListByQuestionIdValidator.Validate(new GetOptionsListByQuestionIdQuery(questionId: 1, tenantId: Guid.NewGuid())).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _getOptionsListByQuestionIdValidator.ShouldHaveValidationErrorFor(x => x.Id, 0);
+        }
+    }
+}
