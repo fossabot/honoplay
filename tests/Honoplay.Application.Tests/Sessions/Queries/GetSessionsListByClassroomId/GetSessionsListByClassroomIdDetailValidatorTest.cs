cecf1c141576128a728cc65f6415fc549d4c61ae
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-21 17:59:38 +0300

    GetSessionsListByClassroomId servis unit testleri yazıldı.

---
+++ tests/Honoplay.Application.Tests/Sessions/Queries/GetSessionsListByClassroomId/GetSessionsListByClassroomIdDetailValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using Honoplay.Application.Sessions.Queries.GetSessionsListByClassroomId;
+using System;
+using Xunit;
+
+namespace Honoplay.Application.Tests.Options.Queries.GetSessionsListByClassroomId
+{
+    public class GetSessionsListByClassroomIdValidatorTest
+    {
+        private readonly GetSessionsListByClassroomIdValidator _getSessionsListByClassroomIdValidator;
+
+        public GetSessionsListByClassroomIdValidatorTest()
+        {
+            _getSessionsListByClassroomIdValidator = new GetSessionsListByClassroomIdValidator();
+        }
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_getSessionsListByClassroomIdValidator.Validate(new GetSessionsListByClassroomIdQuery(classroomId: 1, tenantId: Guid.NewGuid())).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _getSessionsListByClassroomIdValidator.ShouldHaveValidationErrorFor(x => x.ClassroomId, 0);
+        }
+    }
+}
