17af6d04dcb728cfa9324765c9d6469cd007c660
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-21 12:46:32 +0300

    GetTrainingsListByTrainingSeriesId endpoint değiştirildi

--- src/Honoplay.AdminWebAPI/Controllers/TrainingController.cs
+++ src/Honoplay.AdminWebAPI/Controllers/TrainingController.cs
@@ -3,6 +3,7 @@ using Honoplay.Application.Trainings.Commands.CreateTraining;
 using Honoplay.Application.Trainings.Commands.UpdateTraining;
 using Honoplay.Application.Trainings.Queries.GetTrainingDetail;
 using Honoplay.Application.Trainings.Queries.GetTrainingsList;
+using Honoplay.Application.Trainings.Queries.GetTrainingsListByTrainingSeriesId;
 using Honoplay.Common._Exceptions;
 using Honoplay.Common.Extensions;
 using Microsoft.AspNetCore.Authorization;
@@ -145,7 +146,8 @@ namespace Honoplay.AdminWebAPI.Controllers
         /// </summary>
         /// <param name="trainingSeriesId">Get trainings list </param>
         /// <returns>Get training by tenant id and trainingSeries id with status code.</returns>
-        [HttpGet("TrainingSeriesId")]
+        [HttpGet]
+        [Route("GetTrainingsByTrainingSeriesId")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -156,7 +158,7 @@ namespace Honoplay.AdminWebAPI.Controllers
                 var userId = Claims[ClaimTypes.Sid].ToInt();
                 var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
 
-                var trainingsListModel = await Mediator.Send(new GetTrainingDetailQuery(userId, trainingSeriesId, tenantId));
+                var trainingsListModel = await Mediator.Send(new GetTrainingsListByTrainingSeriesIdQuery(userId, trainingSeriesId, tenantId));
 
                 return Ok(trainingsListModel);
             }
