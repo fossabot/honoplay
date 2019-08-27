bb1e4aa8eafe82bd4658893086f598319bad5205
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-28 01:28:07 +0300

    Sınıfları getirmek için TrainingId ve TrainerUserId parametreleri alan servis yazıldı

---
+++ src/Honoplay.Application/Classrooms/Queries/GetClassroomsListByTrainingIdWithTrainerUserId/GetClassroomsListByTrainingIdWithTrainerUserIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using Honoplay.Application._Infrastructure;
+using MediatR;
+using Newtonsoft.Json;
+using System;
+
+namespace Honoplay.Application.Classrooms.Queries.GetClassroomsListByTrainingIdWithTrainerUserId
+{
+    public class GetClassroomsListByTrainingIdWithTrainerUserIdQuery : IRequest<ResponseModel<ClassroomsListByTrainingIdWithTrainerUserIdModel>>
+    {
+        public GetClassroomsListByTrainingIdWithTrainerUserIdQuery(Guid tenantId, int trainingId, int trainerUserId)
+        {
+            TenantId = tenantId;
+            TrainingId = trainingId;
+            TrainerUserId = trainerUserId;
+        }
+
+        public GetClassroomsListByTrainingIdWithTrainerUserIdQuery() { }
+
+        [JsonIgnore]
+        public Guid TenantId { get; }
+        [JsonIgnore]
+        public int TrainerUserId { get; }
+        public int TrainingId { get; set; }
+
+    }
+}
