914eb48df00b377caa2a8b1d77aaa52e4a757d71
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-08-27 18:55:15 +0300

    TrainerAPI için trainerId verildiğinde o trainer'a bağlı training listesini dönen servis yazıldı.

---
+++ src/Honoplay.Application/Trainings/Queries/GetTrainingsListByTrainerId/GetTrainingsListByTrainerUserIdValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Honoplay.Application.Trainings.Queries.GetTrainingsListByTrainerUserId
+{
+    public class GetTrainingsListByTrainerUserIdValidator : AbstractValidator<GetTrainingsListByTrainerUserIdQuery>
+    {
+        public GetTrainingsListByTrainerUserIdValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
