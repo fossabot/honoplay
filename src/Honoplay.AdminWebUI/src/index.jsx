7da10d3c692ffaad1040277c253379e5b0627916
Aga Huseynov <agahuseynov@hotmail.com>
2019-08-27 10:03:32 +0300

    Page content

--- src/Honoplay.AdminWebUI/src/index.jsx
+++ src/Honoplay.AdminWebUI/src/index.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import Login from './views/Login/Login';
-import { init } from '@omegabigdata/terasu-api-proxy';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { projeId } from './helpers/Terasu';
-import store from './redux/store';
-
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import Login from "./views/Login/Login";
+import { init } from "@omegabigdata/terasu-api-proxy";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { projeId } from "./helpers/Terasu";
+import store from "./redux/store";
 
 render(
-    <Provider store={store}>
-        <Router>
-            <Route exact path="/" component={Login} />
-            <Route path="/honoplay" component={App} />
-        </Router>
-    </Provider>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/" component={Login} />
+      <Route path="/honoplay" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
