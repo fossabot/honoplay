f725073cd50726ea8a02cc4523f889757ecd6727
sadumankucuk <sadumankucuk@gmail.com>
2019-09-24 16:57:59 +0300

    option table için kullanılan converter kaldırıldı

--- src/Honoplay.AdminWebUI/src/helpers/Converter.js
+++ src/Honoplay.AdminWebUI/src/helpers/Converter.js
@@ -1,27 +1,18 @@
-export const genderToString = (array) => {
-    array.map(item => {
-        if (item.gender == 0) {
-            item.gender = "Erkek"
-        } else item.gender = "Kadın"
-    })
-    return array;
-}
+export const genderToString = array => {
+  array.map(item => {
+    if (item.gender == 0) {
+      item.gender = 'Erkek';
+    } else item.gender = 'Kadın';
+  });
+  return array;
+};
 
 export const departmentToString = (departmentArray, array) => {
-    array.map(item => {
-        departmentArray.map(department => {
-            if (item.departmentId === department.id) {
-                item.departmentId = department.name;
-            }
-        })
-    })
-}
-
-export const booleanToString = (array) => {
-    array.map(item => {
-        if (item.isCorrect == true) {
-            item.isCorrect = "Doğru"
-        } else item.isCorrect = "Yanlış"
-    })
-    return array;
-}
+  array.map(item => {
+    departmentArray.map(department => {
+      if (item.departmentId === department.id) {
+        item.departmentId = department.name;
+      }
+    });
+  });
+};
