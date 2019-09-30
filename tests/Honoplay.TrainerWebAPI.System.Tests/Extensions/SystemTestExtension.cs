96b84db24b617e19ecf4bccbd1a682a953f15863
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-09-30 11:29:14 +0300

    TrainerWebAPI için integration test projesi oluşturuldu. AdminWebAPI directory değiştirildi. Nugetlar aynı sürüme getirildi.

---
+++ tests/Honoplay.TrainerWebAPI.System.Tests/Extensions/SystemTestExtension.cs
@@ -0,0 +1,30 @@
+﻿using Honoplay.Application.TrainerUsers.Commands.AuthenticateTrainerUser;
+using Honoplay.Common.Constants;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Honoplay.TrainerWebAPI.System.Tests.IntegrationTests.Extensions
+{
+    public static class SystemTestExtension
+    {
+        public static HttpClient GetTokenAuthorizeHttpClient(CustomWebApplicationFactory<Startup> factory)
+        {
+            var client = factory.CreateClient();
+            var authenticateTrainerUserCommand = new AuthenticateTrainerUserCommand
+            {
+                Email = "registered@omegabigdata.com",
+                Password = "Passw0rd"
+            };
+            var content = JsonConvert.SerializeObject(authenticateTrainerUserCommand);
+            var httpResponse = client.PostAsync("api/TrainerUser/Authenticate", new StringContent(content, Encoding.UTF8, StringConstants.ApplicationJson)).Result;
+
+            var token = JsonConvert.DeserializeObject<Dictionary<string, object>>(httpResponse.Content.ReadAsStringAsync().Result)["token"];
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.ToString());
+
+            return client;
+        }
+    }
+}
