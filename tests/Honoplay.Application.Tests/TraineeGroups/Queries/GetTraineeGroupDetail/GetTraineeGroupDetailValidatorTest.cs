c005a86b10bbb7f032425f41d7bffe8209498de5
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-01 17:28:06 +0300

    TraineeGroup için GetDetail servis testi yazıldı.

---
+++ tests/Honoplay.Application.Tests/TraineeGroups/Queries/GetTraineeGroupDetail/GetTraineeGroupDetailValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using Xunit;
+
+namespace Honoplay.Application.Tests.TraineeGroups.Queries.GetTraineeGroupDetail
+{
+    public class GetTraineeGroupDetailValidatorTest : TestBase
+    {
+        private readonly GetTraineeGroupDetailValidator _validator;
+
+        public GetTraineeGroupDetailValidatorTest()
+        {
+            _validator = new GetTraineeGroupDetailValidator();
+        }
+
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_validator.Validate(new GetTraineeGroupDetailQuery(adminUserId: 1, id: 1, tenantId: Guid.NewGuid())).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _validator.ShouldHaveValidationErrorFor(x => x, 0);
+        }
+    }
+}
