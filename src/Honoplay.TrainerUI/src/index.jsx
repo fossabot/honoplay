666cb76c062b04cf5f02c247850a23ec57ad9d80
Aga Huseynov <agahuseynov@hotmail.com>
2019-10-02 13:43:35 +0300

    sevice worker eklendi

--- src/Honoplay.TrainerUI/src/index.jsx
+++ src/Honoplay.TrainerUI/src/index.jsx
@@ -5,20 +5,9 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./Redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./Helpers/Extensions";
+import { register } from "../public/serviceWorker";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(registration => {
-        console.log("SW registered: ", registration);
-      })
-      .catch(registrationError => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
-
+register();
 render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
