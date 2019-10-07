bf97cb5c14d885a5e3d146008dc02cb8a663131e
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-07 13:27:44 +0300

    Proje içinde kullanılacak içerikler için ContentFile tablosu oluşturuldu. TraineeUser ve Question ile ilişki kuruldu.

---
+++ src/Honoplay.Domain/Entities/ContentFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Honoplay.Domain.Entities
+{
+    public class ContentFile : BaseEntity
+    {
+        public ContentFile()
+        {
+            TraineeUsers = new HashSet<TraineeUser>();
+            Questions = new HashSet<Question>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentType { get; set; }
+        public int Size { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+
+        public virtual ICollection<TraineeUser> TraineeUsers { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
