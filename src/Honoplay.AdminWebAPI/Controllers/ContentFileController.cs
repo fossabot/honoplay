9dad69774bad69b57d032d716a5e6de2100948fc
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-08 14:40:07 +0300

    ContentFile için integration testleri ve servisleri yazıldı.

--- src/Honoplay.AdminWebAPI/Controllers/ContentFileController.cs
+++ src/Honoplay.AdminWebAPI/Controllers/ContentFileController.cs
@@ -8,11 +8,117 @@ using System;
 using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Honoplay.Application.ContentFiles.Commands.CreateContentFile;
+using Honoplay.Application.ContentFiles.Commands.UpdateContentFile;
+using Honoplay.Application.ContentFiles.Queries.GetContentFilesList;
 
 namespace Honoplay.AdminWebAPI.Controllers
 {
     public class ContentFileController : BaseController
     {
+
+        /// <summary>
+        /// This service create contentFile.
+        /// </summary>
+        /// <param name="command">Create contentFile model</param>
+        /// <returns>CreateContentFileModel with status code.</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<CreateContentFileModel>>> Post([FromBody]CreateContentFileCommand command)
+        {
+            try
+            {
+                var userId = Claims[ClaimTypes.Sid].ToInt();
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                command.CreatedBy = userId;
+                command.TenantId = tenantId;
+
+                var createContentFileModel = await Mediator.Send(command);
+                return Created($"api/traineeUser", createContentFileModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ObjectAlreadyExistsException ex)
+            {
+                return Conflict(new ResponseModel<CreateContentFileModel>(new Error(HttpStatusCode.Conflict, ex)));
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+
+        /// <summary>
+        /// This service update contentFile.
+        /// </summary>
+        /// <param name="command">Update contentFile model</param>
+        /// <returns>UpdateContentFileModel with status code.</returns>
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ResponseModel<UpdateContentFileModel>>> Put([FromBody]UpdateContentFileCommand command)
+        {
+            try
+            {
+                command.UpdatedBy = Claims[ClaimTypes.Sid].ToInt();
+                command.TenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var updateContentFileModel = await Mediator.Send(command);
+
+                return Ok(updateContentFileModel);
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ObjectAlreadyExistsException ex)
+            {
+                return Conflict(new ResponseModel<UpdateContentFileModel>(new Error(HttpStatusCode.Conflict, ex)));
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+        /// <summary>
+        /// This service retrieve all contentFiles by tenant id. Filtered skip and take parameters.
+        /// </summary>
+        /// <param name="query">GetAll contentFile model</param>
+        /// <returns>Get all contentFiles list by tenant id with status code.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ResponseModel<ContentFilesListModel>>> Get([FromQuery] GetContentFilesListQueryModel query)
+        {
+            try
+            {
+                var tenantId = Guid.Parse(Claims[ClaimTypes.UserData]);
+
+                var contentFilesListModel = await Mediator.Send(new GetContentFilesListQuery(tenantId, query.Skip, query.Take));
+
+                return Ok(contentFilesListModel);
+
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.ToInt());
+            }
+        }
+
         /// <summary>
         /// This service retrieve contentFile.
         /// </summary>
---
+++ tests/Honoplay.AdminWebAPI.System.Tests/Controllers/ContentFileControllerIntegrationTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Honoplay.AdminWebAPI;
+using Honoplay.Application.ContentFiles.Commands.CreateContentFile;
+using Honoplay.Application.ContentFiles.Commands.UpdateContentFile;
+using Honoplay.Application.ContentFiles.Queries.GetContentFilesList;
+using Honoplay.Common.Constants;
+using Honoplay.AdminWebAPI.System.Tests.Extensions;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Honoplay.AdminWebAPI.System.Tests.Controllers
+{
+    public class ContentFileControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public ContentFileControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanCreateContentFileAsync()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var createContentFileCommand = new CreateContentFileCommand
+            {
+                CreateContentFileModels = new List<CreateContentFileCommandModel>
+                {
+                    new CreateContentFileCommandModel
+                    {
+                        Name = "contentFile1",
+                        ContentType = "contentFile1",
+                        Data = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 },
+                    }
+                }
+            };
+
+            var serializedContentFileCommand = JsonConvert.SerializeObject(createContentFileCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PostAsync(requestUri: "/ContentFile",
+                content: new StringContent(content: serializedContentFileCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanUpdateContentFile()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var updateContentFileCommand = new UpdateContentFileCommand
+            {
+                Id = Guid.Parse("4f2b88e3-704c-41d8-a679-f608a159d055"),
+                Name = "contentFile1",
+                ContentType = "contentFile1",
+                Data = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 }
+            };
+
+            var serializedUpdateCommand = JsonConvert.SerializeObject(updateContentFileCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PutAsync(requestUri: "/ContentFile",
+                    content: new StringContent(content: serializedUpdateCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            //Must be successful
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetContentFilesList()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var getContentFilesListQuery = new GetContentFilesListQueryModel
+            {
+                Skip = 0,
+                Take = 10
+            };
+
+            var httpResponse = await authorizedClient.GetAsync(
+                requestUri: $"/ContentFile?Skip={getContentFilesListQuery.Skip}&Take={getContentFilesListQuery.Take}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetContentFileDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"/ContentFile/4f2b88e3-704c-41d8-a679-f608a159d055");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+    }
+}
