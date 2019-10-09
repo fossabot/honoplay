9aa069979ac13ad8a0d7815ddbe5ca728a33520c
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-09 14:20:46 +0300

    TraineeUser için Avatar bilgileri eklendi.

--- src/Honoplay.Application/TraineeUsers/Queries/GetTraineeUserDetail/TraineeDetailUserModel.cs
+++ src/Honoplay.Application/TraineeUsers/Queries/GetTraineeUserDetail/TraineeDetailUserModel.cs
@@ -7,10 +7,6 @@ namespace Honoplay.Application.TraineeUsers.Queries.GetTraineeUserDetail
     public struct TraineeUserDetailModel
     {
         public int Id { get; set; }
-        public int CreatedBy { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
-        public int? UpdatedBy { get; set; }
-        public DateTimeOffset? UpdatedAt { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -19,29 +15,29 @@ namespace Honoplay.Application.TraineeUsers.Queries.GetTraineeUserDetail
         public int Gender { get; set; }
         public int WorkingStatusId { get; set; }
         public int DepartmentId { get; set; }
+        public int? AvatarId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
 
-        private static Expression<Func<TraineeUser, TraineeUserDetailModel>> Projection
+        private static Expression<Func<TraineeUser, TraineeUserDetailModel>> Projection => traineeUser => new TraineeUserDetailModel
         {
-            get
-            {
-                return traineeUser => new TraineeUserDetailModel
-                {
-                    Id = traineeUser.Id,
-                    Email = traineeUser.Email,
-                    CreatedAt = traineeUser.CreatedAt,
-                    CreatedBy = traineeUser.CreatedBy,
-                    UpdatedBy = traineeUser.UpdatedBy,
-                    UpdatedAt = traineeUser.UpdatedAt,
-                    Gender = traineeUser.Gender,
-                    PhoneNumber = traineeUser.PhoneNumber,
-                    NationalIdentityNumber = traineeUser.NationalIdentityNumber,
-                    Surname = traineeUser.Surname,
-                    Name = traineeUser.Name,
-                    WorkingStatusId = traineeUser.WorkingStatusId,
-                    DepartmentId = traineeUser.DepartmentId
-                };
-            }
-        }
+            Id = traineeUser.Id,
+            Email = traineeUser.Email,
+            CreatedAt = traineeUser.CreatedAt,
+            CreatedBy = traineeUser.CreatedBy,
+            UpdatedBy = traineeUser.UpdatedBy,
+            UpdatedAt = traineeUser.UpdatedAt,
+            Gender = traineeUser.Gender,
+            PhoneNumber = traineeUser.PhoneNumber,
+            NationalIdentityNumber = traineeUser.NationalIdentityNumber,
+            Surname = traineeUser.Surname,
+            Name = traineeUser.Name,
+            WorkingStatusId = traineeUser.WorkingStatusId,
+            DepartmentId = traineeUser.DepartmentId,
+            AvatarId = traineeUser.AvatarId
+        };
 
         public static TraineeUserDetailModel Create(TraineeUser traineeUser)
         {
