4f21164e1ca9c3ce74b6801ab8140dfde7d8798a
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-14 13:18:40 +0300

    CreateTag için servis metodunun unit testi yazıldı. Avatar için refactoring yapıldı.

---
+++ tests/Honoplay.Application.Tests/Tags/Commands/CreateTag/CreateTagValidatorTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Honoplay.Application.Tags.Commands.CreateTag;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Honoplay.Application.Tests.Tags.Commands.CreateTag
+{
+    public class CreateTagValidatorTest : AbstractValidator<CreateTagCommand>
+    {
+        private readonly CreateTagValidator _createTagValidator;
+
+        public CreateTagValidatorTest()
+        {
+            _createTagValidator = new CreateTagValidator();
+        }
+
+        [Fact]
+        public void ShouldBeValid()
+        {
+            var newTag = new List<CreateTagCommandModel>
+            {
+                new CreateTagCommandModel
+                {
+                    Name = "sample",
+                    QuestionId = 1
+                }
+            };
+            Assert.True(_createTagValidator.Validate(new CreateTagCommand { CreateTagModels = newTag }).IsValid);
+        }
+
+    }
+}
