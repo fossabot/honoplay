61e58d631543b0756c875c7a892a6868e9175e53
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-14 11:25:09 +0300

    Veritabanı nesneleri oluşturuldu.

---
+++ src/Honoplay.Domain/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Honoplay.Domain.Entities
+{
+    public class Tag : BaseEntity
+    {
+        public Tag()
+        {
+            QuestionTags = new HashSet<QuestionTag>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<QuestionTag> QuestionTags { get; set; }
+    }
+}
