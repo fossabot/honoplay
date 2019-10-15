5c93fff79234ce905d77d371a2f6f3aaea3dc556
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-15 11:25:56 +0300

    Tag servisi oluşturuldu. Tag servisinin integration testleri oluşturuldu. Integration testler için veriler oluşturuldu.

---
+++ tests/Honoplay.AdminWebAPI.System.Tests/Controllers/TagControllerIntegrationTests.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using Honoplay.AdminWebAPI;
+using Honoplay.Application.Tags.Commands.CreateTag;
+using Honoplay.Application.Tags.Commands.UpdateTag;
+using Honoplay.Application.Tags.Queries.GetTagsList;
+using Honoplay.Common.Constants;
+using Honoplay.AdminWebAPI.System.Tests.Extensions;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Honoplay.AdminWebAPI.System.Tests.Controllers
+{
+    public class TagControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public TagControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanCreateTagAsync()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var createTagCommand = new CreateTagCommand
+            {
+                CreateTagModels = new List<CreateTagCommandModel>
+                {
+                    new CreateTagCommandModel
+                    {
+                        Name = "tag1",
+                        ToQuestion = true
+                    }
+                }
+            };
+
+            var serializedTagCommand = JsonConvert.SerializeObject(createTagCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PostAsync(requestUri: "/Tag",
+                content: new StringContent(content: serializedTagCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanUpdateTag()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var updateTagCommand = new UpdateTagCommand
+            {
+                Id = 1,
+                Name = "taggg",
+                ToQuestion = true
+            };
+
+            var serializedUpdateCommand = JsonConvert.SerializeObject(updateTagCommand);
+
+            // The endpoint or route of the controller action.
+            var httpResponse = await authorizedClient.PutAsync(requestUri: "/Tag",
+                    content: new StringContent(content: serializedUpdateCommand,
+                    encoding: Encoding.UTF8,
+                    mediaType: StringConstants.ApplicationJson));
+
+            //Must be successful
+            httpResponse.EnsureSuccessStatusCode();
+
+            Assert.True(httpResponse.IsSuccessStatusCode);
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetTagsList()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var getTagsListQuery = new GetTagsListQueryModel
+            {
+                Skip = 0,
+                Take = 10
+            };
+
+            var httpResponse = await authorizedClient.GetAsync(
+                requestUri: $"/Tag?Skip={getTagsListQuery.Skip}&Take={getTagsListQuery.Take}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetTagDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"/Tag/1");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+        [Fact]
+        public async Task CanGetTagsListByQuestionIdDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"/Question/1/Tag");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+    }
+}
