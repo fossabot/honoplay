437eb72f3f73851d8546b9c9df2000dec3849bb6
Aga Huseynov <agahuseynov@hotmail.com>
2019-10-15 13:48:39 +0300

    Store.js den Persist kaldirildi

--- src/Honoplay.TrainerUI/src/Redux/store.js
+++ src/Honoplay.TrainerUI/src/Redux/store.js
@@ -3,18 +3,7 @@ import { addReducerToList } from "@omegabigdata/honoplay-redux-helper/Src/reduce
 import { createStore, applyMiddleware, compose } from "redux";
 
 import thunk from "redux-thunk";
+const newReducers = addReducerToList({});
+const store = createStore(newReducers, {}, compose(applyMiddleware(thunk)));
 
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-
-const persistConfig = {
-  key: "root",
-  storage
-};
-
-const persistedReducer = persistReducer(persistConfig, addReducerToList({}));
-
-let store = createStore(persistedReducer, {}, compose(applyMiddleware(thunk)));
-let persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
