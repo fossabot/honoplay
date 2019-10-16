0209461ddea177e7a0129b8a84a3d8acd3843e8b
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 12:54:11 +0300

    QuestionType için integration testler yazıldı.

---
+++ tests/Honoplay.AdminWebAPI.System.Tests/Controllers/QuestionTypeControllerIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using Honoplay.AdminWebAPI.System.Tests.Extensions;
+using Honoplay.Application.QuestionTypes.Queries.GetQuestionTypesList;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Honoplay.AdminWebAPI.System.Tests.Controllers
+{
+    public class QuestionTypeControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public QuestionTypeControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanGetQuestionTypesList()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var getQuestionTypesListQuery = new GetQuestionTypesListQueryModel
+            {
+                Skip = 0,
+                Take = 10
+            };
+
+            var httpResponse = await authorizedClient.GetAsync(
+                requestUri: $"/QuestionType?Skip={getQuestionTypesListQuery.Skip}&Take={getQuestionTypesListQuery.Take}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetQuestionTypeDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"/QuestionType/1");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+    }
+}
