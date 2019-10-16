0216bd1dd487d71259704c39f83848fad977092d
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 13:51:41 +0300

    QuestionDifficulty veritabanı nesnesi oluşturuldu.

---
+++ src/Honoplay.Domain/Entities/QuestionDifficulty.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Honoplay.Domain.Entities
+{
+    public class QuestionDifficulty
+    {
+        public QuestionDifficulty()
+        {
+            Questions = new HashSet<Question>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
