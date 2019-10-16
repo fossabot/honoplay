434fe6f685439d779c8fb0a91149caca82578b87
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 12:42:14 +0300

    QuestionType için GetList servis metodu yazıldı.

---
+++ src/Honoplay.Application/QuestionTypes/Queries/GetQuestionTypesList/GetQuestionTypesListQuery.cs
@@ -0,0 +1,23 @@
+﻿using Honoplay.Application._Infrastructure;
+using MediatR;
+
+namespace Honoplay.Application.QuestionTypes.Queries.GetQuestionTypesList
+{
+    public class GetQuestionTypesListQuery : IRequest<ResponseModel<QuestionTypesListModel>>
+    {
+        public GetQuestionTypesListQuery(int? skip, int? take)
+        {
+            Skip = skip;
+            Take = take;
+        }
+
+        public int? Skip { get; private set; }
+        public int? Take { get; private set; }
+
+    }
+    public class GetQuestionTypesListQueryModel : IRequest<ResponseModel<QuestionTypesListModel>>
+    {
+        public int? Skip { get; set; }
+        public int? Take { get; set; }
+    }
+}
---
+++ src/Honoplay.Application/QuestionTypes/Queries/GetQuestionTypesList/QuestionTypesListModel.cs
@@ -0,0 +1,28 @@
+﻿using Honoplay.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace Honoplay.Application.QuestionTypes.Queries.GetQuestionTypesList
+{
+    public struct QuestionTypesListModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static Expression<Func<QuestionType, QuestionTypesListModel>> Projection
+        {
+            get
+            {
+                return questionType => new QuestionTypesListModel
+                {
+                    Id = questionType.Id,
+                    Name = questionType.Name
+                };
+            }
+        }
+        public static QuestionTypesListModel Create(QuestionType questionType)
+        {
+            return Projection.Compile().Invoke(questionType);
+        }
+    }
+}
