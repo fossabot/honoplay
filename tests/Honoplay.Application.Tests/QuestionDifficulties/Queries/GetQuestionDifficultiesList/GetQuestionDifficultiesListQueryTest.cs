d572b08edd7ce97625d6011ab2923b7d05c6fbb7
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 14:29:52 +0300

    QuestionDifficulties GetList servis metodunun unit testlerine refactoring yapıldı.

--- tests/Honoplay.Application.Tests/QuestionDifficulties/Queries/GetQuestionDifficultiesList/GetQuestionDifficultiesListQueryTest.cs
+++ tests/Honoplay.Application.Tests/QuestionDifficulties/Queries/GetQuestionDifficultiesList/GetQuestionDifficultiesListQueryTest.cs
@@ -63,7 +63,7 @@ namespace Honoplay.Application.Tests.QuestionDifficulties.Queries.GetQuestionDif
             };
             context.Questions.Add(question);
 
-            var questionDifficulty = new QuestionDifficulty
+            var questionDifficulty = new Domain.Entities.QuestionDifficulty
             {
                 Id = 1,
                 Name = "questionDifficulty1",
