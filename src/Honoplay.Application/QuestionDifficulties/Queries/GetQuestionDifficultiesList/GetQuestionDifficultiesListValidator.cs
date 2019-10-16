eaa8ce0ec7c87075b46cdd8bfdef943e336ae668
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 14:26:46 +0300

    QuestionDifficulty için GetList servis metodunun Validator, RequestHandler RequestModel, ResponseModel sınıfları yazıldı.

---
+++ src/Honoplay.Application/QuestionDifficulties/Queries/GetQuestionDifficultiesList/GetQuestionDifficultiesListValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Honoplay.Application.QuestionDifficulties.Queries.GetQuestionDifficultiesList
+{
+    public class GetQuestionDifficultiesListValidator : AbstractValidator<GetQuestionDifficultiesListQueryModel>
+    {
+        public GetQuestionDifficultiesListValidator()
+        {
+            RuleFor(x => x.Skip)
+                .GreaterThan(-1);
+
+            RuleFor(x => x.Take)
+                .GreaterThan(4)
+                .LessThan(101);
+        }
+    }
+}
