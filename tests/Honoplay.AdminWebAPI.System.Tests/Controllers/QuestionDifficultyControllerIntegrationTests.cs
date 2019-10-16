3be8d1c8cc411b71d2bfe11c65f58512a0ed437b
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 14:42:15 +0300

    QuestionDifficulty için integration testler yazıldı.

---
+++ tests/Honoplay.AdminWebAPI.System.Tests/Controllers/QuestionDifficultyControllerIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using Honoplay.AdminWebAPI.System.Tests.Extensions;
+using Honoplay.Application.QuestionDifficulties.Queries.GetQuestionDifficultiesList;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Honoplay.AdminWebAPI.System.Tests.Controllers
+{
+    public class QuestionDifficultyControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public QuestionDifficultyControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CanGetQuestionDifficultiesList()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var getQuestionDifficultiesListQuery = new GetQuestionDifficultiesListQueryModel
+            {
+                Skip = 0,
+                Take = 10
+            };
+
+            var httpResponse = await authorizedClient.GetAsync(
+                requestUri: $"/QuestionDifficulty?Skip={getQuestionDifficultiesListQuery.Skip}&Take={getQuestionDifficultiesListQuery.Take}");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task CanGetQuestionDifficultyDetail()
+        {
+            var authorizedClient = SystemTestExtension.GetTokenAuthorizeHttpClient(_factory);
+
+            var httpResponse = await authorizedClient.GetAsync(requestUri: $"/QuestionDifficulty/1");
+
+            httpResponse.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, httpResponse.StatusCode);
+        }
+    }
+}
