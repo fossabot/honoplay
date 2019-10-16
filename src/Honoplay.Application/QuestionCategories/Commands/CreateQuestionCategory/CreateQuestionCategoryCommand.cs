6fbf9ecea84c3ce4c4fbcaf4156affd790f6a7d5
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 17:02:12 +0300

    QuestionCategory için Create servisinin Command sınıfı yazıldı.

---
+++ src/Honoplay.Application/QuestionCategories/Commands/CreateQuestionCategory/CreateQuestionCategoryCommand.cs
@@ -0,0 +1,16 @@
+﻿using Honoplay.Application._Infrastructure;
+using MediatR;
+using Newtonsoft.Json;
+using System;
+
+namespace Honoplay.Application.QuestionCategories.Commands.CreateQuestionCategory
+{
+    public class CreateQuestionCategoryCommand : IRequest<ResponseModel<CreateQuestionCategoryModel>>
+    {
+        [JsonIgnore]
+        public int CreatedBy { get; set; }
+        [JsonIgnore]
+        public Guid TenantId { get; set; }
+        public string Name { get; set; }
+    }
+}
