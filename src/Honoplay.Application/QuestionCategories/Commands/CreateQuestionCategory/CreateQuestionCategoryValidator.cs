30e154476ce6850376cef3f488dde501e35123e8
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 17:02:46 +0300

    QuestionCategory için Create servisinin Validator sınıfı yazıldı.

---
+++ src/Honoplay.Application/QuestionCategories/Commands/CreateQuestionCategory/CreateQuestionCategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Honoplay.Application.QuestionCategories.Commands.CreateQuestionCategory
+{
+    public class CreateQuestionCategoryValidator : AbstractValidator<CreateQuestionCategoryCommand>
+    {
+        public CreateQuestionCategoryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
