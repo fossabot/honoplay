0235b2ac227a1c13eec191db617a800d311d3af4
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 12:19:07 +0300

    QuestionType için GetList servis metodunun Validator unit testi yazıldı.

---
+++ tests/Honoplay.Application.Tests/QuestionTypes/Queries/GetQuestionTypesList/GetQuestionTypesListValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Honoplay.Application.Tests.QuestionType.Queries.GetQuestionTypeList
+{
+    public class GetQuestionTypeListValidatorTest
+    {
+        private readonly GetQuestionTypeListValidator _getQuestionTypeListValidator;
+
+        public GetQuestionTypeListValidatorTest()
+        {
+            _getQuestionTypeListValidator = new GetQuestionTypeListValidator();
+        }
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_getQuestionTypeListValidator.Validate(new GetQuestionTypeListQueryModel { Take = 5 }).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValid()
+        {
+            _getQuestionTypeListValidator.ShouldHaveValidationErrorFor(x => x.Skip, -1);
+            _getQuestionTypeListValidator.ShouldHaveValidationErrorFor(x => x.Take, 3);
+            _getQuestionTypeListValidator.ShouldHaveValidationErrorFor(x => x.Take, 102);
+        }
+    }
+}
