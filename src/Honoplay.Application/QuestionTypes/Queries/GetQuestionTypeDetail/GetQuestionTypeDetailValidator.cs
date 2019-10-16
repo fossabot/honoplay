08c09a5bde2e98573a8b31ca18cfda0604ffbf1e
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 12:25:31 +0300

    QuestionType için GetDetail servis metodunun Validatoru yazıldı.

---
+++ src/Honoplay.Application/QuestionTypes/Queries/GetQuestionTypeDetail/GetQuestionTypeDetailValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Honoplay.Application.QuestionTypes.Queries.GetQuestionTypeDetail
+{
+    public class GetQuestionTypeDetailValidator : AbstractValidator<GetQuestionTypeDetailQuery>
+    {
+        public GetQuestionTypeDetailValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
