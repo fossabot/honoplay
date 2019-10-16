dc4c3244f0d28c06869b3cbc3f536f7754470dbc
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-16 16:46:15 +0300

    QuestionCategory için GetDetail servis metodunun Validator unit testi yazıldı.

---
+++ tests/Honoplay.Application.Tests/QuestionCategory/Queries/GetQuestionCategoryDetail/GetQuestionCategoryDetailValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentValidation.TestHelper;
+using Honoplay.Application.QuestionCategories.Queries.GetQuestionCategoryDetail;
+using Xunit;
+
+namespace Honoplay.Application.Tests.QuestionCategorys.Queries.GetQuestionCategoryDetail
+{
+    public class GetQuestionCategoryDetailValidatorTest
+    {
+        private readonly GetQuestionCategoryDetailValidator _getQuestionCategoryDetailValidator;
+
+        public GetQuestionCategoryDetailValidatorTest()
+        {
+            _getQuestionCategoryDetailValidator = new GetQuestionCategoryDetailValidator();
+        }
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_getQuestionCategoryDetailValidator.Validate(new GetQuestionCategoryDetailQuery(adminUserId: 1, id: Guid.NewGuid(), tenantId: Guid.NewGuid())).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _getQuestionCategoryDetailValidator.ShouldHaveValidationErrorFor(x => x.Id, Guid.Empty);
+        }
+    }
+}
