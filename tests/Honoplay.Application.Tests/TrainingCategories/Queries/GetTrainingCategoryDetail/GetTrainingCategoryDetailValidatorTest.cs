5a5432801f0677dd1e390d3fbc92680704c1d689
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-23 14:26:21 +0300

    TrainingCategory için GetDetail servis metotlarının unit testleri yazıldı.

---
+++ tests/Honoplay.Application.Tests/TrainingCategories/Queries/GetTrainingCategoryDetail/GetTrainingCategoryDetailValidatorTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.TestHelper;
+using Honoplay.Application.TraininigCategories.Queries.GetTraininigCategoryDetail;
+using Xunit;
+
+namespace Honoplay.Application.Tests.TraininigCategories.Queries.GetTraininigCategoryDetail
+{
+    public class GetTraininigCategoryDetailValidatorTest
+    {
+        private readonly GetTraininigCategoryDetailValidator _getTraininigCategoryDetailValidator;
+
+        public GetTraininigCategoryDetailValidatorTest()
+        {
+            _getTraininigCategoryDetailValidator = new GetTraininigCategoryDetailValidator();
+        }
+        [Fact]
+        public void ShouldBeValid()
+        {
+            Assert.True(_getTraininigCategoryDetailValidator.Validate(new GetTraininigCategoryDetailQuery(id: 1)).IsValid);
+        }
+
+        [Fact]
+        public void ShouldBeNotValidForNullOrEmpty()
+        {
+            _getTraininigCategoryDetailValidator.ShouldHaveValidationErrorFor(x => x.Id, 0);
+        }
+    }
+}
