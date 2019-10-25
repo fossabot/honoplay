8881749f983af3b04b0fb566d4b48fd8cc77d86b
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-25 18:44:56 +0300

    AdminWebAPI için Question servis metodunun Update işlemi Tag eklenebilmesi için değiştirildi.

--- src/Honoplay.Application/Questions/Commands/UpdateQuestion/UpdateQuestionCommand.cs
+++ src/Honoplay.Application/Questions/Commands/UpdateQuestion/UpdateQuestionCommand.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace Honoplay.Application.Questions.Commands.UpdateQuestion
 {
@@ -12,6 +13,7 @@ namespace Honoplay.Application.Questions.Commands.UpdateQuestion
         public int Duration { get; set; }
         public int? DifficultyId { get; set; }
         public int? CategoryId { get; set; }
+        public ICollection<int> TagsIdList { get; set; }
         public int? TypeId { get; set; }
         public Guid? ContentFileId { get; set; }
         [JsonIgnore]
