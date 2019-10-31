de02c21652a4d67d39dfad5e6f379931f49d3d41
bilgin <bbilginn@gmail.com>
2019-10-31 16:24:05 +0300

    cpu-ram limitleri

--- src/Honoplay.AdminWebAPI/deploy/integration.sefa.sh
+++ src/Honoplay.AdminWebAPI/deploy/integration.sefa.sh
@@ -3,11 +3,17 @@
 SERVICE_NAME=honoplay_admin_webapi_service_app
 IMAGE_NAME=admin-api-service
 
+# nproc komutu cekirdek sayisini verir.
+LIMIT_CPU=$(( $(nproc) / 4 ))
+LIMIT_MEMORY=1000m
+
 if docker service ls | grep -q ${SERVICE_NAME}; then
 	echo "$(tput setaf 2) ${SERVICE_NAME} Bulundu Guncelleniyor...$(tput sgr 0)"
 	
     docker service update \
         --image ${DOCKER_REGISTRY}/${IMAGE_NAME}:${BUILD_NUMBER}teamcity \
+        --limit-cpu $LIMIT_CPU \
+        --limit-memory $LIMIT_MEMORY \
 		${SERVICE_NAME}
 else
 	echo "$(tput setaf 3) ${SERVICE_NAME} Bulunamadi Kuruluyor...$(tput sgr 0)"
@@ -23,6 +29,8 @@ else
 		--env REDIS_CACHE_SERVICE=honoplay_redis_cache_service \
 		--env DC=DC-1 \
         --mount type=bind,source=/root/honoplay_static_files,destination=/app/wwwroot \
+        --limit-cpu $LIMIT_CPU \
+        --limit-memory $LIMIT_MEMORY \
         --constraint 'node.labels.is-application-node == true' \
         ${DOCKER_REGISTRY}/${IMAGE_NAME}:${BUILD_NUMBER}teamcity
 fi
--- src/Honoplay.TraineeWebAPI/deploy/integration.sefa.sh
+++ src/Honoplay.TraineeWebAPI/deploy/integration.sefa.sh
@@ -3,11 +3,17 @@
 SERVICE_NAME=honoplay_trainee_webapi_service_app
 IMAGE_NAME=trainee-api-service
 
+# nproc komutu cekirdek sayisini verir.
+LIMIT_CPU=$(( $(nproc) / 4 ))
+LIMIT_MEMORY=1000m
+
 if docker service ls | grep -q ${SERVICE_NAME}; then
 	echo "$(tput setaf 2) ${SERVICE_NAME} Bulundu Guncelleniyor...$(tput sgr 0)"
 	
     docker service update \
         --image ${DOCKER_REGISTRY}/${IMAGE_NAME}:${BUILD_NUMBER}teamcity \
+        --limit-cpu $LIMIT_CPU \
+        --limit-memory $LIMIT_MEMORY \
 		${SERVICE_NAME}
 else
 	echo "$(tput setaf 3) ${SERVICE_NAME} Bulunamadi Kuruluyor...$(tput sgr 0)"
@@ -20,6 +26,8 @@ else
         --network frontend \
 		--env TZ=Asia/Istanbul \
 		--env REDIS_CACHE_SERVICE=honoplay_redis_cache_service \
+        --limit-cpu $LIMIT_CPU \
+        --limit-memory $LIMIT_MEMORY \
         --constraint 'node.labels.is-application-node == true' \
         ${DOCKER_REGISTRY}/${IMAGE_NAME}:${BUILD_NUMBER}teamcity
 fi
