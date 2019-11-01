8f26b0635ff849c5e2f1176837f5b2ea397b2cf6
emrekas <y.emrekas@outlook.com>
2019-11-01 15:22:07 -0700

    QuestionTag için Question servisinin Create metodu düzeltildi.

--- src/Honoplay.Application/Questions/Commands/CreateQuestion/CreateQuestionModel.cs
+++ src/Honoplay.Application/Questions/Commands/CreateQuestion/CreateQuestionModel.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Honoplay.Application.Tags.Queries.GetTagDetail;
+using System;
 using System.Collections.Generic;
-using Honoplay.Domain.Entities;
 
 namespace Honoplay.Application.Questions.Commands.CreateQuestion
 {
@@ -11,13 +11,13 @@ namespace Honoplay.Application.Questions.Commands.CreateQuestion
         public int Duration { get; set; }
         public int? DifficultyId { get; set; }
         public int? CategoryId { get; set; }
-        public ICollection<Tag> Tags { get; set; }
+        public ICollection<TagDetailModel> Tags { get; set; }
         public int? TypeId { get; set; }
         public Guid? ContentFileId { get; set; }
         public int CreatedBy { get; private set; }
         public DateTimeOffset CreatedAt { get; private set; }
 
-        public CreateQuestionModel(int id, string text, int duration, int? difficultyId, int? categoryId, ICollection<Tag> tags, int? typeId, Guid? contentFileId, int createdBy, DateTimeOffset createdAt)
+        public CreateQuestionModel(int id, string text, int duration, int? difficultyId, int? categoryId, ICollection<TagDetailModel> tags, int? typeId, Guid? contentFileId, int createdBy, DateTimeOffset createdAt)
         {
             Id = id;
             Text = text;
